231405cb7c5f53b05693b152e06f92d9f89219d2
Andre Pierre <andre.laurence94@gmail.com>
2020-08-13 04:36:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -113,11 +113,11 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash = {
-   :alan_kay[:known_for] => "GUI" 
+
+   alans_new_info = "GUI" 
    
-   
-}
+  programmer_hash[:alan_kay][:known_for] =  alans_new_info
+
 programmer_hash
 end
 
